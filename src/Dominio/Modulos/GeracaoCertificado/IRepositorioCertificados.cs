a2c927b849c8adede4202fe8b2eaaf7033a9e83c
Kauan Silva <kauanmacedo338@gmail.com>
2026-09-16 16:55:23 -0300

    add: implenta irepositorio da camada de dominio

---
+++ src/Dominio/Modulos/GeracaoCertificado/IRepositorioCertificados.cs
@@ -0,0 +1,4 @@
+using GeradorCertificados.Dominio.Compartilhado;
+using GeradorCertificados.Dominio.Modulos.GeracaoCertificado;
+
+public interface IRepositorioCertificados : IRepositorio<Certificado>;
