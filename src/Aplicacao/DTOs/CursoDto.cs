1b8859ae3e7adaad551e330224cfceb41f558c67
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-09-21 20:06:58 -0300

    Add: Cursos DTOs

--- src/Aplicacao/DTOs/CursoDto.cs
+++ src/Aplicacao/DTOs/CursoDto.cs
@@ -1,6 +1,7 @@
 namespace GeradorCertificados.Aplicacao.DTOs;
 
-public sealed record CadastrarCursoDto(
+public sealed record CursoDto(
+    Guid Id,
     string Nome,
     string? Descricao,
     uint CargaHoraria,
